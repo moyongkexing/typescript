a81ead4d4960be386d031bedc39343c10ec6f0cc
moyongkexing <moyongkexing@gmail.com>
2021-02-14 15:45:22 +0900

    Intersection Types

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 import { number, string } from 'yargs';
 
-interface USER {
+interface NAME {
   first: string;
   last: string | null;
 }
 
-let userObj: USER = { first: "Yamada", last: null };
+let userObj: NAME = { first: "Yamada", last: null };
 
 const func1 = (x: number, y:number): number => {
   return x + y;
 }
+// Intersection Types
+type PROFILE = {
+  age: number;
+  city: string;
+};
+
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+type USER = PROFILE & LOGIN;
+
+const userA: USER = {
+  age: 30,
+  city: "Tokyo",
+  username: "xxx",
+  password: "yyy",
+};
+
+
 function App() {
   return (
     <div className="App">
