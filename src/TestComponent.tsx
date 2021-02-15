8a5575508b279dc5846d3c995d41c9e013fde56f
moyongkexing <moyongkexing@gmail.com>
2021-02-15 16:00:25 +0900

    EventHandler: データ型

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -10,10 +10,17 @@ interface userData {
 const TestComponent:React.FC<Props> = (props) => {
   const [count, setCount] = useState<number | null>(null);
   const [user, setUser] = useState<userData>({id:1, name: "dummy"});
+  const [inputData, setInputData] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData(e.target.value);
+  }
   return (
     <div>
       <h1>{props.text}</h1>
       <h1>{count}</h1>
+      <input type="text" value={inputData} onChange={handleInputChange} />
+      <p>{ inputData }</p>
     </div>
   );
 }
